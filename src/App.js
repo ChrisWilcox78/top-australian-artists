9c52db43ad110bc309fca8036b50e4f8f7861524
Chris Wilcox <christopher.wilcox@orionhealth.com>
2018-04-22 14:29:06 +1000

    Introduced a basic fetch from last.fm and displayed raw JSON.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this._updateStateWithArtists = this._updateStateWithArtists.bind(this);
+    
+    this.state = {
+      topArtists: null
+    };
+  }
+
+  _updateStateWithArtists(data) {
+    this.setState ({
+      topArtists: data.topartists.artist
+    });
+  }
+
+  componentDidMount() {
+    fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=australia&limit=10&api_key=24c990f786fd892d7c74a62505a795c8&format=json')
+    .then(results => results.json())
+    .then(this._updateStateWithArtists);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">Australia's Top Artists</h1>
+          <div>Powered by</div>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            {this.state.topArtists ? JSON.stringify(this.state.topArtists): 'Retrieving top artists...'}
         </p>
       </div>
     );
