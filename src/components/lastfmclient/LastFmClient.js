4882dc096d18da4eeb6689e7982c514c1c93ddfe
Chris Wilcox <christopher.wilcox@orionhealth.com>
2018-04-23 19:57:35 +1000

    fixed some issue with routing and request urls that caused problems when deployed to github pages; added some padding

--- src/components/lastfmclient/LastFmClient.js
+++ src/components/lastfmclient/LastFmClient.js
@@ -2,7 +2,7 @@
 class LastFmClient {
 	getTopArtists(updateCallback) {
 		fetch(
-			"http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=australia&limit=10&api_key=24c990f786fd892d7c74a62505a795c8&format=json"
+			"https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=australia&limit=10&api_key=24c990f786fd892d7c74a62505a795c8&format=json"
 		)
 		.then(results => results.json())
 		.then(data => updateCallback(data.topartists.artist));
@@ -10,7 +10,7 @@ class LastFmClient {
 
 	getArtistDetails(mbid, updateCallback) {
 		fetch(
-			"http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=24c990f786fd892d7c74a62505a795c8&format=json&mbid=" +
+			"https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=24c990f786fd892d7c74a62505a795c8&format=json&mbid=" +
 			mbid
 		)
 		.then(results => results.json())
